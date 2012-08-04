14733877e7ed2454d14b8e5c5abdbd24cda73b0d
Zhang Cheng <StephenPCG@gmail.com>
2012-08-05 02:49:48 +0800

    Initial copy of init.sh

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## create symlinks for vimrc
+ln -s .vim/vimrc $HOME/.vimrc
+
+## init and submodules
+git submodule init
+git submodule update
+
+## install required tools
+
+
+## some plugins needs additional compile stuffs
