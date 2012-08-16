8698b265a60d4844a29834ec114af6e854c54d8a
Zhang Cheng <StephenPCG@gmail.com>
2012-08-16 23:45:17 +0800

    forget to add files/ in last commit

---
+++ files/skeletons/general.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2011 <+AUTHOR+> <<+EMAIL+>>
+# 
+# Author:     <+AUTHOR+> <<+EMAIL+>>
+# Maintainer: <+AUTHOR+> <<+EMAIL+>>
+
+<+CURSOR+>
+
+# vim:ai:et:sts=4:sw=4:
---
+++ files/skeletons/general.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+<+CURSOR+>
+
+# vim:ai:et:sts=4:tw=80:sw=4:
